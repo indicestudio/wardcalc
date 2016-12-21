83eda19b714849df359d76e7a138fb3568af14c6
Indicestudio <indicestudio@gmail.com>
2016-12-21 18:39:14 -0300

    fectch basic implementation

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-console.log('It works!')
+fetch('https://na.api.pvp.net/api/lol/las/v1.3/stats/by-summoner/rozencr4nts/summary?api_key=RGAPI-51f23220-8bb5-4d88-951e-d9eea135c076')
+  .then(response => response.json())
+  .then(data => console.log(data));
